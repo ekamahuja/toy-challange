a259765a2eac09d3771836a591ee4862b2fcfda6
Ekam <ekamahujau@gmail.com>
2021-12-27 20:51:48 +1000

    Added report button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
-// document.querySelector("#y-0-x-0").innerHTML = "R"
 let currentPosition;
 let xAxis = document.querySelector('#x-axis-input');
 let yAxis = document.querySelector('#y-axis-input');
 let fAxis = document.querySelector('#f-axis-input');
-
 let arrowInputs = document.querySelector("#arrow-input").children; 
+const reportBtn = document.querySelector('#report-btn');
+const manualInputBtn = document.querySelector('#submit-manual-input');
+
+
+
+reportBtn.addEventListener('click', () => {
+    alert(`X-Axis: ${currentPosition.xAxis}<br>Y-Axis: ${currentPosition.yAxis}<br>Facing: ${currentPosition.fAxis.replace("f-", "").charAt(0).toUpperCase()+currentPosition.fAxis.slice(3)}`, "success")
+})
+
 for (let i = 0; i < arrowInputs.length; i++) {
     arrowInputs[i].addEventListener("click", event => {
         const direction = arrowInputs[i].id;
@@ -31,8 +38,6 @@ for (let i = 0; i < arrowInputs.length; i++) {
     });
 };
 
-
-const manualInputBtn = document.querySelector('#submit-manual-input');
 manualInputBtn.addEventListener('click', event => {
     xAxis.disabled = true;
     yAxis.disabled = true;
@@ -57,10 +62,15 @@ function resetPosition() {
     i.classList.remove(currentPosition.fAxis);
 };
 
-async function setPosition(xPosition, yPosition, fPosition) {
+async function setPosition(xPosition, yPosition, fPosition) { 
+    if (xPosition > 4) return alert("Cannot move toy robot over X5 axis!", "danger");
+    if (xPosition < 0) return alert("Cannot move toy robot lower than X0 axis!", "danger");
+    if (yPosition > 4) return alert("Cannot move toy robot over Y5 axis!", "danger");
+    if (yPosition < 0) return alert("Cannot move toy robot lower than Y0 axis!", "danger");
     if (currentPosition) resetPosition();
+
     let newPosition = document.querySelector(`#y-${yPosition}-x-${xPosition}`);
-    newPosition.innerHTML = "R";
+    newPosition.innerHTML = "R";    
     newPosition.classList.add(fPosition);
     currentPosition = {
         xAxis: xPosition,
@@ -69,3 +79,15 @@ async function setPosition(xPosition, yPosition, fPosition) {
         fAxis: fPosition
     };
 };
+
+
+function alert(message, type) {
+    let alert = document.createElement('div');
+    alert.innerHTML = `<div class="alert alert-${type} alert-dismissible" role="alert">${message}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
+    document.querySelector("#notice").appendChild(alert);
+    setTimeout(function () {    
+        alert.remove();
+    }, 2500)
+}
+
+manualInputBtn.click();
