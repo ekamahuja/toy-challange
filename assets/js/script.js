4bdb808c91e3c8f2e7abffc47703fa0a94156ab2
Ekam <ekamahujau@gmail.com>
2021-12-27 16:46:37 +1000

    Provided ids to all boxes within the gameboard so it easier to manipulate the robbot using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.querySelector("#origin").innerHTML = "R"
+// document.querySelector("#origin").innerHTML = "R"
 
 
 const manualInputBtn = document.querySelector('#submit-manual-input');
@@ -13,7 +13,7 @@ manualInputBtn.addEventListener('click', event => {
     manualInputBtn.disabled = true;
     manualInputBtn.innerHTML = 'Loading...'
 
-
+    document.querySelector(`body > div.game-board.mt-5 > div:nth-child(${xAxis.value})`).innerHTML = "R"
 
 
    console.log(`${xAxis.value}\n${yAxis.value}\n${fAxis.value}`)
