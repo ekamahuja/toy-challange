f1d0e75d14804fc81f79d5589dcb4a42e095c074
Ekam <ekamahujau@gmail.com>
2021-12-27 19:54:10 +1000

    Arrow inputs can now also control the position of the robot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,71 @@
 // document.querySelector("#y-0-x-0").innerHTML = "R"
 let currentPosition;
+let xAxis = document.querySelector('#x-axis-input');
+let yAxis = document.querySelector('#y-axis-input');
+let fAxis = document.querySelector('#f-axis-input');
+
+let arrowInputs = document.querySelector("#arrow-input").children; 
+for (let i = 0; i < arrowInputs.length; i++) {
+    arrowInputs[i].addEventListener("click", event => {
+        const direction = arrowInputs[i].id;
+        let currentX = parseInt(currentPosition.xAxis);
+        let currentY = parseInt(currentPosition.yAxis);
+        switch(direction) {
+            case "up":
+                currentY = currentY + 1;
+                break;
+            case "down":
+                currentY = currentY - 1;
+                break;
+            case "left":
+                currentX = currentX - 1;
+                break;
+            case "right":
+                currentX = currentX + 1;
+                break;
+        };
+        setPosition(currentX, currentY, currentPosition.fAxis).then(() => {
+            xAxis.value = currentPosition.xAxis;
+            yAxis.value = currentPosition.yAxis;
+        })
+    });
+};
+
 
 const manualInputBtn = document.querySelector('#submit-manual-input');
 manualInputBtn.addEventListener('click', event => {
-    let xAxis = document.querySelector('#x-axis-input');
-    let yAxis = document.querySelector('#y-axis-input');
-    let fAxis = document.querySelector('#f-axis-input');
-
     xAxis.disabled = true;
     yAxis.disabled = true;
     fAxis.disabled = true;
     manualInputBtn.disabled = true;
     manualInputBtn.innerHTML = 'Loading...'
 
-    if (currentPosition) {
-        let i = document.querySelector(`${currentPosition.xYPositionId}`);
-        i.innerHTML = '';
-        i.classList.remove(currentPosition.fAxis);
-    }
-
-    let newPosition = document.querySelector(`#y-${yAxis.value}-x-${xAxis.value}`);
-    newPosition.innerHTML = "R";
-    newPosition.classList.add(`${fAxis.value}`);
-    currentPosition = {
-        xYPositionId: `#y-${yAxis.value}-x-${xAxis.value}`,
-        fAxis: fAxis.value
-    };
+    setPosition(xAxis.value, yAxis.value, fAxis.value).then(() => {
+        xAxis.disabled = false;
+        yAxis.disabled = false;
+        fAxis.disabled = false;
+        manualInputBtn.disabled = false;
+        manualInputBtn.innerHTML = 'Submit'
+    });
 
 
-    xAxis.disabled = false;
-    yAxis.disabled = false;
-    fAxis.disabled = false;
-    manualInputBtn.disabled = false;
-    manualInputBtn.innerHTML = 'Submit'
-    console.log(currentPosition);
 });
+
+function resetPosition() {
+    let i = document.querySelector(`${currentPosition.xYPositionId}`);
+    i.innerHTML = '';
+    i.classList.remove(currentPosition.fAxis);
+};
+
+async function setPosition(xPosition, yPosition, fPosition) {
+    if (currentPosition) resetPosition();
+    let newPosition = document.querySelector(`#y-${yPosition}-x-${xPosition}`);
+    newPosition.innerHTML = "R";
+    newPosition.classList.add(fPosition);
+    currentPosition = {
+        xAxis: xPosition,
+        yAxis: yPosition,
+        xYPositionId: `#y-${yPosition}-x-${xPosition}`,
+        fAxis: fPosition
+    };
+};
