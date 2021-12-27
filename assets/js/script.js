806bdc1e70b3a91bf12821f66270da027290c46d
Ekam <ekamahujau@gmail.com>
2021-12-27 21:05:06 +1000

    Added move button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,50 @@ let currentPosition;
 let xAxis = document.querySelector('#x-axis-input');
 let yAxis = document.querySelector('#y-axis-input');
 let fAxis = document.querySelector('#f-axis-input');
-let arrowInputs = document.querySelector("#arrow-input").children; 
+let arrowInputs = document.querySelector("#arrow-input").children;
+const moveBtn = document.querySelector('#move-btn');
 const reportBtn = document.querySelector('#report-btn');
 const manualInputBtn = document.querySelector('#submit-manual-input');
 
-
+moveBtn.addEventListener("click", () => {
+    direction = currentPosition.fAxis;
+    direction = direction.replace("f-", ""); 
+    moveInDirection(direction);
+});
 
 reportBtn.addEventListener('click', () => {
-    alert(`X-Axis: ${currentPosition.xAxis}<br>Y-Axis: ${currentPosition.yAxis}<br>Facing: ${currentPosition.fAxis.replace("f-", "").charAt(0).toUpperCase()+currentPosition.fAxis.slice(3)}`, "success")
-})
+    alert(`X-Axis: ${currentPosition.xAxis}<br>Y-Axis: ${currentPosition.yAxis}<br>Facing: ${currentPosition.fAxis.replace("f-", "").charAt(0).toUpperCase() + currentPosition.fAxis.slice(3)}`, "success");
+});
 
 for (let i = 0; i < arrowInputs.length; i++) {
     arrowInputs[i].addEventListener("click", event => {
         const direction = arrowInputs[i].id;
-        let currentX = parseInt(currentPosition.xAxis);
-        let currentY = parseInt(currentPosition.yAxis);
-        switch(direction) {
-            case "up":
-                currentY = currentY + 1;
-                break;
-            case "down":
-                currentY = currentY - 1;
-                break;
-            case "left":
-                currentX = currentX - 1;
-                break;
-            case "right":
-                currentX = currentX + 1;
-                break;
-        };
-        setPosition(currentX, currentY, currentPosition.fAxis).then(() => {
-            xAxis.value = currentPosition.xAxis;
-            yAxis.value = currentPosition.yAxis;
-        })
+        moveInDirection(direction);
     });
 };
+
+function moveInDirection(direction) {
+    let currentX = parseInt(currentPosition.xAxis);
+    let currentY = parseInt(currentPosition.yAxis);
+    switch (direction) {
+        case "north":
+            currentY = currentY + 1;
+            break;
+        case "south":
+            currentY = currentY - 1;
+            break;
+        case "west":
+            currentX = currentX - 1;
+            break;
+        case "east":
+            currentX = currentX + 1;
+            break;
+    };
+    setPosition(currentX, currentY, currentPosition.fAxis).then(() => {
+        xAxis.value = currentPosition.xAxis;
+        yAxis.value = currentPosition.yAxis;
+    })
+}
 
 manualInputBtn.addEventListener('click', event => {
     xAxis.disabled = true;
@@ -62,15 +71,15 @@ function resetPosition() {
     i.classList.remove(currentPosition.fAxis);
 };
 
-async function setPosition(xPosition, yPosition, fPosition) { 
-    if (xPosition > 4) return alert("Cannot move toy robot over X5 axis!", "danger");
-    if (xPosition < 0) return alert("Cannot move toy robot lower than X0 axis!", "danger");
-    if (yPosition > 4) return alert("Cannot move toy robot over Y5 axis!", "danger");
-    if (yPosition < 0) return alert("Cannot move toy robot lower than Y0 axis!", "danger");
+async function setPosition(xPosition, yPosition, fPosition) {
+    if (xPosition > 4) return alert("Cannot move toy robot over X-5 axis!", "danger");
+    if (xPosition < 0) return alert("Cannot move toy robot lower than X-0 axis!", "danger");
+    if (yPosition > 4) return alert("Cannot move toy robot over Y-5 axis!", "danger");
+    if (yPosition < 0) return alert("Cannot move toy robot lower than Y-0 axis!", "danger");
     if (currentPosition) resetPosition();
 
     let newPosition = document.querySelector(`#y-${yPosition}-x-${xPosition}`);
-    newPosition.innerHTML = "R";    
+    newPosition.innerHTML = "R";
     newPosition.classList.add(fPosition);
     currentPosition = {
         xAxis: xPosition,
@@ -85,7 +94,7 @@ function alert(message, type) {
     let alert = document.createElement('div');
     alert.innerHTML = `<div class="alert alert-${type} alert-dismissible" role="alert">${message}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
     document.querySelector("#notice").appendChild(alert);
-    setTimeout(function () {    
+    setTimeout(function () {
         alert.remove();
     }, 2500)
 }
